90f684c9d627b8b0ea2fc542a8a3da69c3c8a343
RAFFAEL AUGUSTO SIQUEIRA BINO <26155232@sempreunama.com.br>
2020-03-05 10:16:08 -0300

    Add codigo fonte e arquivo para trabalho

---
+++ aulaflutter05_03_2020.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Home());
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "App hello world",
+      home: Scaffold(
+        appBar: AppBar(),
+      ),
+    );
+  }
+}
